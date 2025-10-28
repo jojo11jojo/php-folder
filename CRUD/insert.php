2f85d87b77ec682d2a46278e8ac3a9c41c419ef7
jojo11jojo <silpallab1204@gmail.com>
2025-10-29 00:56:12 +0530

    add two more file

---
+++ CRUD/insert.php
@@ -0,0 +1,54 @@
+<?php
+include 'connect.php';
+if (isset($_POST['submit'])) {
+  $a = $_POST['name'];
+  $b = $_POST['mobile'];
+  $c = $_POST['email'];
+  $d = $_POST['password'];
+
+  // Always quote your SQL query string
+  $sql = "INSERT INTO `student` (`id`, `name`,`mob`, `email`,`pass`)  VALUES (NULL,'$a', '$b', $c, '$d')";
+  $r = mysqli_query($con, $sql);
+  if ($r) {
+    echo "Record inserted successfully!";
+  } else {
+    echo "Error: " . mysqli_error($con);
+  }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>insert</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+<body>
+    <form method="post">
+         <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Name</label>
+    <input type="name" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text"></div>
+  </div>
+   <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">MOB</label>
+    <input type="number" name="mobile" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text"></div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
+</body>
+</html>
---
+++ CRUD/navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+<body>
+    <ul class="nav nav-pills nav-fill">
+  <li class="nav-item">
+    <a class="nav-link" aria-current="page" href="insert.php">Sign up</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Update</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Delete</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Display</a>
+  </li>
+</ul>
+    
+</body>
+</html>
