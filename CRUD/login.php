f37a76f913e3e820016a13d452d0664d06a31896
jojo11jojo <silpallab1204@gmail.com>
2025-10-30 01:34:40 +0530

    modification

--- CRUD/login.php
+++ CRUD/login.php
@@ -1,6 +1,17 @@
 <?php
 include 'connect.php';
 ?>
+<?php
+ if(isset($_POST['submit'])){
+    
+ }
+   
+ 
+$con->close();
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,24 +26,16 @@ include 'connect.php';
   <li class="nav-item">
     <a class="nav-link" aria-current="page" href="insert.php">Dashbord</a>
   </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Update</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Delete</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="display.php" tabindex="-1" aria-disabled="true">Basic Details</a>
-  </li>
+
 </ul>
     <form method="post">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email"  name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
   </div>
   <button type="submit" class="btn btn-primary">SUBMIT</button>
 </form>
