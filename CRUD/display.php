4380694a997344ff120992a3d00018fded0cff0a
jojo11jojo <silpallab1204@gmail.com>
2025-10-30 00:45:58 +0530

    modification

--- CRUD/display.php
+++ CRUD/display.php
@@ -3,6 +3,31 @@ include 'connect.php';
 include 'navbar.php';
 ?>
 <?php
+$dname=$dpass=$dmobile=$did=$demail="...........................";
+
+ if(isset($_POST['submit'])){
+    $input=$_POST['einput'];
+
+    $sql = "SELECT * FROM student where mob=$input";
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+       
+            $did="Id: ".$row['id'];
+             $dname="Name: ".$row['name'];
+             $dmobile="Mobile: ".$row['mob'];
+              $demail="Email: ".$row['email'];
+            $dpass="Password: ".$row['pass'];
+           
+
+    }
+} else {
+    echo "0 results";
+}
+
+$con->close();
+ }
 
 ?>
 <!DOCTYPE html>
@@ -17,12 +42,9 @@ include 'navbar.php';
 <body>
     <div class="container px-5 my-5">
     <form class="d-flex justify-content-center text-aligne-center" id="contactForm" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-        <select class="form-select w-50 h-25 mx-5 bg-primary text-light" aria-label="Default select example" name="selected">
-            <option value="Name" selected>Name</option>
-            <option value="Mobile No.">Mobile</option>
-        </select>
         <div class="mb-3 w-100 mx-5">
-            <input class="form-control" type="text" name="einput" placeholder="Enter here..."/>
+            <label for="">MOB</label>
+            <input class="form-control" type="number" name="einput" placeholder="Enter Mobile no ,here..."/>
         </div>
         <div class="mb-3 w-12.5">
             <input class="btn btn-primary" type="submit" value="Submit" name=submit>
@@ -34,15 +56,15 @@ include 'navbar.php';
     Your Information
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item"><?php echo $did;?></li>
-    <li class="list-group-item"><?php echo $dname;?></li>
-      <li class="list-group-item"><?php echo $dmobile;?></li>
-        <li class="list-group-item"><?php echo $demail;?></li>
+  <li class="list-group-item"><?php echo $did;?></li> 
+  <li class="list-group-item"><?php echo $dname;?></li>
+  <li class="list-group-item"><?php echo $dmobile;?></li>
+  <li class="list-group-item"><?php echo $demail;?></li>
     <li class="list-group-item"><?php echo $dpass;?></li>
-  
   </ul>
 </div>
 
     
 </body>
 </html>
+
