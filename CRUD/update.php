cbaa042980d3c74b23b16a5f124b2111bc97ff09
jojo11jojo <silpallab1204@gmail.com>
2025-11-03 18:00:16 +0530

    add update.php

---
+++ CRUD/update.php
@@ -0,0 +1,24 @@
+<?php
+include "connect.php";
+include "navbar.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>update</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+<body>
+    <form method="post" >
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Enter ID No: </label>
+    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <button type="submit" class="btn btn-primary">Search</button>
+</form>
+    
+</body>
+</html>
